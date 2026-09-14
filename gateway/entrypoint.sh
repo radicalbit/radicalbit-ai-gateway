6a120233a2824201a5fcef1ff3f9302d567889c9
mauro.cortellazzi <mauro.cortellazzi@radicalbit.io>
2026-09-14 14:50:40 +0200

    feat(chore): add uvicorn headers to run behind a proxy

--- gateway/entrypoint.sh
+++ gateway/entrypoint.sh
@@ -1,4 +1,6 @@
 # Support workers via environment variable (default: 1)
 WORKERS="${UVICORN_WORKERS:-1}"
 
-exec uvicorn --host="$HTTP_INTERFACE" --port "$HTTP_PORT" --workers "$WORKERS" radicalbit_ai_gateway.server:app --loop uvloop
+exec uvicorn --host="$HTTP_INTERFACE" --port "$HTTP_PORT" --workers "$WORKERS" \
+     --proxy-headers --forwarded-allow-ips='*' \
+     radicalbit_ai_gateway.server:app --loop uvloop
